c2c19c724c58a17d67a257d83047392743170f9c
rachel490 <ccori12@naver.com>
2022-01-25 12:12:09 +0900

    Fix: api 함수 수정

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,9 @@ const { default: axios } = require('axios');
 // getExchangeRate
 const getExchangeRate = (countries) => {
   const params = countries.length ? `&currencies=${countries.join()}` : '';
-  axios.get(`http://api.currencylayer.com/live?access_key=${process.env.REACT_APP_API_KEY}${params}`);
+  return axios.get(
+    `http://api.currencylayer.com/live?access_key=${process.env.REACT_APP_API_KEY}${params}`,
+  );
 };
 
 export default getExchangeRate;
