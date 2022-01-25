33dd78a791926698091072b45ecfe14794595371
rachel490 <ccori12@naver.com>
2022-01-26 01:48:32 +0900

    Refactor: 공통 constants와 함수로 변경

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,8 @@
-const convertPrice = (stringPrice) => {
+export const convertPrice = (stringPrice) => {
   const decimal = Number(stringPrice).toFixed(2).replace(/(^\d*[.]\d{3}$) | ([^0-9.]) |(^\d*[.]{2})/, '');
   const formatted = decimal.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
   return formatted;
 };
-
-export default convertPrice;
 
 export const convertExchangeRate = (params) => {
   const { sendingRate, receivingRate } = params;
