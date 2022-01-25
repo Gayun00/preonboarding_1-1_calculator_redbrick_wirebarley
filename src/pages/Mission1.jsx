f37583ce50356ec20e34a63aa428c09d883849c2
rachel490 <ccori12@naver.com>
2022-01-25 13:40:35 +0900

    Feat: mission1 기본 레이아웃
    Fix: api key 수정
    
    Feat: mission1 데이터 fetching
    
    Fix: converPrice 오류 수정
    
    Feat: 환율 표시 기능 구현
    
    Fix: api 함수 오류 수정
    
    Feat: 환율 계산 기능 구현
    
    Feat: 송금액 validation 추가
    
    Style: 스타일 추가
    
    Refactor: validation 로직을 별도 함수로 분리
    
    handleSubmit의 함수 이름도 handleRemit으로 변경
    
    Refactor: validation 메세지 세분화
    
    Style: validation height값  추가
    
    [Fix] mission2내용 삭제

--- src/pages/Mission1.jsx
+++ src/pages/Mission1.jsx
@@ -1,7 +1,85 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { useState, useEffect, useRef } from 'react/cjs/react.development';
+import api from '../api';
+import convertPrice from '../utils';
+import styles from './Mission1.module.css';
 
 function Mission1() {
-  return <div>Mission1</div>;
+  const [exchangeRates, setExchangeRates] = useState({});
+  const [selectedCountry, setSelectedCountry] = useState('KRW');
+  const [convertedPrice, setConvertedPrice] = useState(0);
+  const [isValid, setIsValid] = useState(true);
+  const [validationMessage, setValidationMessage] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    api(['KRW', 'JPY', 'PHP']).then((res) => setExchangeRates(res.data.quotes));
+  }, []);
+
+  const selectCountry = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+
+  const validateRemit = (val) => {
+    if (Number(val) < 0) {
+      setIsValid(false);
+      setValidationMessage('0이상의 값을 넣어주세요');
+    } else if (Number(val) > 10000) {
+      setIsValid(false);
+      setValidationMessage('10000이하의 값을 넣어주세요');
+    } else if (Number.isNaN(Number(val))) {
+      setIsValid(false);
+      setValidationMessage('숫자만 입력해주세요');
+    } else {
+      setIsValid(true);
+    }
+  };
+
+  const handleRemit = (e) => {
+    e.preventDefault();
+    const val = inputRef.current.value;
+    validateRemit(val);
+    const rate = exchangeRates[`USD${selectedCountry}`];
+    const result = convertPrice(String(rate * val));
+    setConvertedPrice(result);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>환율 계산</h1>
+      <div>
+        <div className={styles.item}>송금국가: 미국(USD)</div>
+        <div>
+          <label htmlFor="reception" className={styles.item}>
+            수취국가:
+            <select name="reception" id="reception" onChange={selectCountry}>
+              <option value="KRW">한국(KRW)</option>
+              <option value="JPY">일본(JPY)</option>
+              <option value="PHP">필리핀(PHP)</option>
+            </select>
+          </label>
+        </div>
+        <div>
+          <p className={styles.item}>
+            환율 :
+            {`${convertPrice(String(exchangeRates[`USD${selectedCountry}`]))} ${selectedCountry}`}
+            /USD
+          </p>
+        </div>
+        <form className={styles.item} onSubmit={handleRemit}>
+          <label htmlFor="remit">
+            송금액:
+            <input type="text" id="remit" ref={inputRef} />
+            <span>USD</span>
+          </label>
+          <button type="submit" className={styles.submitBtn}>Submit</button>
+        </form>
+        <p className={styles.item}>{`수취금액은 ${isValid ? convertedPrice : 0} ${selectedCountry}입니다.`}</p>
+        <p className={styles.validation}>{isValid ? '' : `${validationMessage}`}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Mission1;
